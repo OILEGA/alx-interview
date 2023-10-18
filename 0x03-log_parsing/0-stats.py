c62933a8f41424fba684d58f5316ada6e51409e5
OILEGA <get2mena@gmail.com>
2023-10-18 11:18:09 +0100

    a script that reads stdin line by line and computes metrics

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 import sys
 possible_status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
 totalFilesize = 0
@@ -35,3 +34,4 @@ try:
 
 except KeyboardInterrupt:
         (print_status())
+
