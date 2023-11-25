c7c8cf2d2a23fd241ff145163353df93e2eee2d0
OILEGA <get2mena@gmail.com>
2023-11-25 12:40:33 +0100

    interview making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
-
-"""Determines the fewest number of coins needed to meet a given
-   amount total when given a pile of coins of different values.
+"""Change making module.
 """
 
-def makeChange(coins: list, total:int) -> int:
-    sorted_coins = sorted(coins, reverse=True)
-    used_coins = 0
-    for coin in sorted_coins:
-        if total == 0:  
-            break
-        used_coins += total // coin
-        total = total % coin
-        if total == 0:
-            return used_coins
-        return - 1
 
-print(makeChange([1,2,25], 37))
-print(makeChange([1256, 54, 48, 16, 102], 1453))  
+def makeChange(coins, total):
+    """Determines the fewest number of coins needed to meet a given
+    amount total when given a pile of coins of different values.
+    """
+    if total <= 0:
+        return 0
+    rem = total
+    coins_count = 0
+    coin_idx = 0
+    sorted_coins = sorted(coins, reverse=True)
+    n = len(coins)
+    while rem > 0:
+        if coin_idx >= n:
+            return -1
+        if rem - sorted_coins[coin_idx] >= 0:
+            rem -= sorted_coins[coin_idx]
+            coins_count += 1
+        else:
+            coin_idx += 1
+    return coins_count
